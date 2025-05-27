84189a4fa5eec96a527fb0fa0cf3fba3bc9e5711
leilaweicman <leilaweic@hotmail.com>
2025-05-27 20:26:06 +0200

    Fix: remove absolute base URL from Vite config to fix dynamic imports

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: 'https://portfolio-production-00d9.up.railway.app/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
