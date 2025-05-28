86caab015dfb34ecf9d5567cf7d81a5747d2d962
leilaweicman <leilaweic@hotmail.com>
2025-05-28 13:41:17 +0200

    Add build options

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
     root: '.',
@@ -14,6 +15,11 @@ export default defineConfig({
         react(),
     ],
     build: {
+        rollupOptions: {
+            input: {
+                about: resolve(__dirname, 'resources/js/Pages/About.jsx'),
+            },
+          },
         manifest: true,
         outDir: 'public/build',
         emptyOutDir: true,
