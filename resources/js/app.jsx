e107dfe0c820477ff2f70c80a236d45d3b41cf6d
leilaweicman <leilaweic@hotmail.com>
2025-05-28 09:45:15 +0200

    Fix: unify page resolution using absolute paths

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,17 +5,15 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
-const pages = import.meta.env.PROD
-  ? import.meta.glob('/app/resources/js/Pages/**/*.jsx')
-  : import.meta.glob('./Pages/**/*.jsx');
+const pages = import.meta.glob('/resources/js/Pages/**/*.jsx');
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => {
-        const path = `./Pages/${name}.jsx`;
-        console.log('⏎ Resolving:', path);
+        const path = `/resources/js/Pages/${name}.jsx`;
+        console.log('Resolving:', path);
         return resolvePageComponent(path, pages);
     },
     setup({ el, App, props }) {
